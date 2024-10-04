811129a3f0a8b7742b436bb657a1626161a53a0e
devmcdonough <devmcdonough@gmail.com>
2024-10-04 15:18:40 -0400

    Changes to secret key

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w#yyu($2^fhq1^x4z75_o=8ri(-bw!+zi0&x)t$!qne)y-xifm'
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
